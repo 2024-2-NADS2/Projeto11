77cd1edc724262ecd36555dd22caae3f51c3b094
Hebert Esteves <hebertesteves14.sp@gmail.com>
2024-10-29 00:01:34 -0300

    Atualização da classe AgendamentoColeta

--- src/Backend/src/AgendamentoColeta.js
+++ src/Backend/src/AgendamentoColeta.js
@@ -1,107 +1,128 @@
-import Usuario from './Usuario.js'; 
+const Usuario = require('./Usuario.js');
 
-export default class AgendamentoColeta extends Usuario {
+class AgendamentoColeta extends Usuario {
     constructor(nome, cpf, email, telefone, cep, uf, cidade, endereco, numero, complemento, bairro, pontoColeta, produto, dataAgendada) {
-        super(nome, cpf, email, telefone);       
-        this.cep = cep;                 
-        this.uf = uf;                   
-        this.cidade = cidade;           
-        this.endereco = endereco;       
-        this.numero = numero;           
-        this.complemento = complemento; 
-        this.bairro = bairro;           
-        this.pontoColeta = pontoColeta; 
-        this.produto = produto;         
-        this.dataAgendada = dataAgendada; 
+        super(nome, cpf, email, telefone);
+        this.cep = cep;
+        this.uf = uf;
+        this.cidade = cidade;
+        this.endereco = endereco;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.pontoColeta = pontoColeta;
+        this.produto = produto;
+        this.dataAgendada = dataAgendada;
+        this.dataColetada = null;
     }
 
-     // Gets
-     getCep() {
-        return this.cep;
+    // Métodos Get
+    getCep() { 
+        return this.cep; 
     }
 
-    getUf() {
-        return this.uf;
+    getUf() { 
+        return this.uf; 
     }
 
-    getCidade() {
-        return this.cidade;
+    getCidade() { 
+        return this.cidade; 
     }
 
-    getEndereco() {
-        return this.endereco;
+    getEndereco() { 
+        return this.endereco; 
     }
 
-    getNumero() {
-        return this.numero;
+    getNumero() { 
+        return this.numero; 
     }
 
-    getComplemento() {
-        return this.complemento;
+    getComplemento() { 
+        return this.complemento; 
     }
 
-    getBairro() {
-        return this.bairro;
+    getBairro() { 
+        return this.bairro; 
     }
 
     getPontoColeta() {
-        return this.pontoColeta;
+         return this.pontoColeta; 
     }
 
-    getProduto() {
-        return this.produto;
+    getProduto() { 
+        return this.produto; 
     }
 
-    getDataAgendada() {
-        return this.dataAgendada;
+    getDataAgendada() { 
+        return this.dataAgendada; 
     }
 
-    // Sets
-    setCep(cep) {
-        this.cep = cep;
+    getDataColetada() { 
+        return this.dataColetada; 
     }
 
-    setUf(uf) {
-        this.uf = uf;
+    // Métodos Set
+    setCep(cep) { 
+        this.cep = cep; 
     }
 
-    setCidade(cidade) {
-        this.cidade = cidade;
+    setUf(uf) { 
+        this.uf = uf; 
     }
 
-    setEndereco(endereco) {
-        this.endereco = endereco;
+    setCidade(cidade) { 
+        this.cidade = cidade; 
     }
 
-    setNumero(numero) {
-        this.numero = numero;
+    setEndereco(endereco) { 
+        this.endereco = endereco; 
     }
 
-    setComplemento(complemento) {
-        this.complemento = complemento;
+    setNumero(numero) { 
+        this.numero = numero; 
     }
 
-    setBairro(bairro) {
-        this.bairro = bairro;
+    setComplemento(complemento) { 
+        this.complemento = complemento; 
+    }
+    setBairro(bairro) { 
+        this.bairro = bairro; 
     }
 
-    setPontoColeta(pontoColeta) {
-        this.pontoColeta = pontoColeta;
+    setPontoColeta(pontoColeta) { 
+        this.pontoColeta = pontoColeta; 
     }
 
-    setProduto(produto) {
-        this.produto = produto;
+    setProduto(produto) { 
+        this.produto = produto; 
     }
 
-    setDataAgendada(dataAgendada) {
-        this.dataAgendada = dataAgendada;
+    setDataAgendada(dataAgendada) { 
+        this.dataAgendada = dataAgendada; 
+    }
+
+    setDataColetada(dataColetada) { 
+        this.dataColetada = dataColetada; 
     }
 
     confirmarAgendamento() {
-        console.log(`Agendamento confirmado para ${this.nome}.`);
-        console.log(`Endereço: ${this.endereco}, ${this.numero}, ${this.complemento}, ${this.bairro}, ${this.cidade} - ${this.uf}, CEP: ${this.cep}`);
-        console.log(`Ponto de coleta: ${this.pontoColeta}`);
-        console.log(`Produto: ${this.produto}`);
-        console.log(`Data agendada: ${this.dataAgendada}`);
+        console.log("Dados recebidos para inserção:", {
+            nome: this.getNome(),
+            cpf: this.getCpf(),
+            email: this.getEmail(),
+            telefone: this.getTelefone(),
+            cep: this.getCep(),
+            uf: this.getUf(),
+            cidade: this.getCidade(),
+            endereco: this.getEndereco(),
+            numero: this.getNumero(),
+            complemento: this.getComplemento(),
+            bairro: this.getBairro(),
+            pontoColeta: this.getPontoColeta(),
+            produto: this.getProduto(),
+            dataAgendada: this.getDataAgendada()
+        });
     }
 }
+
+module.exports = AgendamentoColeta;
