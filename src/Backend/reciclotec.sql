4087960f8fee804e0b4955d2cd74378a4874bb7f
Hebert Esteves <hebertesteves14.sp@gmail.com>
2024-10-29 00:11:59 -0300

    Subindo banco de dados atualizado

--- src/Backend/reciclotec.sql
+++ src/Backend/reciclotec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 20/10/2024 às 02:14
+-- Tempo de geração: 29/10/2024 às 03:08
 -- Versão do servidor: 8.0.40
 -- Versão do PHP: 8.2.18
 
@@ -29,7 +29,12 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `agendamento`;
 CREATE TABLE IF NOT EXISTS `agendamento` (
-  `cep` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `cep` varchar(10) NOT NULL,
   `uf` varchar(2) NOT NULL,
   `cidade` varchar(255) NOT NULL,
   `endereco` varchar(255) NOT NULL,
@@ -38,8 +43,10 @@ CREATE TABLE IF NOT EXISTS `agendamento` (
   `bairro` varchar(255) NOT NULL,
   `pontoColeta` varchar(255) NOT NULL,
   `produto` varchar(255) NOT NULL,
-  `dataAgendada` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `dataAgendada` date NOT NULL,
+  `dataColetada` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -56,7 +63,17 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `telefone` varchar(15) DEFAULT NULL,
   `senha` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `cpf`, `email`, `telefone`, `senha`) VALUES
+(4, 'Hebert', '90878362513', 'hebert@gmail.com', '1199987432', '$2b$10$xnao0spSo5Yxy4RcmvoOwe0xF9VjsvpxOSftNMWJ3PcTWODxcDz2i'),
+(11, 'Perla dos Reis Oliveira', '12311323123', 'perlla.sp@terra.com.br', '(11) 98607-7083', '$2b$10$RM/kMOwhDoJIdlGPXc5Xx./dM7VcIyyKBbdbT8y/9gsR5SWzJ2QkC'),
+(12, 'Neymar', '21031891283', 'ney@gmail.com', '1192318293291', '$2b$10$6HwlH4q5gYbAIsPwcFPXkeL86o8Z3rLMjmeVshKP3qGO1zSyFmF5K'),
+(13, 'TEREZO ESTEVES', '38917582390', 'terezo.sp@gmail.com', '11999525491', '$2b$10$ptvISdP8VBbngCKvcgjAOeU6JXDeWIAM1jXwFLVnaCvX.lrIdl7eS');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
